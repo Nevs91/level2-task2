1b8fe97eccdb4ef46ccce0b73ef6aa7abb03b4f4
Sven van Putten <svenvanputten@gmail.com>
2020-09-11 14:54:02 +0200

    Created an questionAdapter and data class for creating, storing and showing questions

---
+++ app/src/main/java/com/example/madlevel2task2/adapters/QuestionAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.madlevel2task2.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task2.R
+import com.example.madlevel2task2.classes.Question
+import com.example.madlevel2task2.databinding.ItemQuestionBinding
+
+class QuestionAdapter(private val questions: List<Question>) : RecyclerView.Adapter<QuestionAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemQuestionBinding.bind(itemView)
+
+        fun databind(question: Question) {
+            binding.tvQuestion.text = question.question
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_question, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(questions[position])
+    }
+
+    override fun getItemCount(): Int {
+        return questions.size
+    }
+}
---
+++ app/src/main/java/com/example/madlevel2task2/classes/Question.kt
@@ -0,0 +1,16 @@
+package com.example.madlevel2task2.classes
+
+data class Question(
+    var question: String,
+    var answer: String
+) {
+    companion object {
+        val QUESTIONS = arrayOf(
+            "",
+        )
+
+        val QUESTION_ANSWERS = arrayOf(
+            "",
+        )
+    }
+}
